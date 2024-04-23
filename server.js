9db1809391784067a2c4a44f0ef05cf1933eb568
dhtprbs12 <dhtprbs12@gmail.com>
2024-04-23 16:58:23 -0700

    fixed download

--- server.js
+++ server.js
@@ -206,13 +206,16 @@ app.post('/create-payment-intent', async (req, res) => {
 app.get('/download/:id', (req, res) => {
 	const productName = req.params.id
 	const docPath = path.join(__dirname, `/assets/${productName}.zip`)
+	const file = fs.createWriteStream(docPath)
 	console.log(`Start Dowloading: ${productName}.zip...`)
-	res.download(docPath, `${productName}.zip`, function (err) {
-		if (err) {
-			// if the file download fails, we throw an error
-			res.status(500).send(new Error(err))
-			console.log(`Error While Dowloading: ${productName}.zip...`)
-		}
+	var stat = fs.statSync(file)
+	res.writeHead(200, { 'Content-Length': stat.size })
+	var fReadStream = fs.createReadStream(file)
+	fReadStream.pipe(res)
+	fReadStream.on('error', (err) => {
+		// log the error and prematurely end the response stream
+		console.log(`Error While Dowloading: ${productName}.zip...`)
+		res.status(500).send(new Error(err))
 	})
 	console.log(`End Dowloading: ${productName}.zip...`)
 })
